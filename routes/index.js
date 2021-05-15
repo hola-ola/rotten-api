78f7008614ec2fcc9201edcc4a7ad85bf1cd4446
André de Albuquerque <itstheandre@gmail.com>
2021-05-15 14:54:05 +0200

    Pushed to gh

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get("/", (req, res, next) => {
   res.json({ user: "ola" });
 });
 
+const moviesRouter = require("./movies");
+
+router.use("/movies", moviesRouter);
+
 module.exports = router;
