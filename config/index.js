dd386631dc6730047c130121aa1d4b30fbb26897
André de Albuquerque <itstheandre@gmail.com>
2021-05-15 12:35:10 +0200

    Lesgo

---
+++ config/index.js
@@ -0,0 +1,21 @@
+// We reuse this import in order to have access to the `body` property in requests
+const express = require("express");
+
+// ℹ️ Responsible for the messages you see in the terminal as requests are coming in
+// https://www.npmjs.com/package/morgan
+const logger = require("morgan");
+
+// ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
+// https://www.npmjs.com/package/cookie-parser
+const cookieParser = require("cookie-parser");
+
+// Middleware configuration
+module.exports = (app) => {
+  // In development environment the app logs
+  app.use(logger("dev"));
+
+  // To have access to `body` property in the request
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+};
